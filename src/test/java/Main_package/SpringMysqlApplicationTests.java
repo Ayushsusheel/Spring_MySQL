94d077ad44871e4874dc9c1a0ac79ad2aabb4aa1
Ayush S <60178558+Ayushsusheel@users.noreply.github.com>
2025-03-20 00:05:53 +0530

    Update SpringMysqlApplicationTests.java

--- src/test/java/Main_package/SpringMysqlApplicationTests.java
+++ src/test/java/Main_package/SpringMysqlApplicationTests.java
@@ -4,7 +4,7 @@ import   org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
  
 @SpringBootTest
-class SpringMysqlApplicationTests {
+class SpringMysqlApplicationTests { 
 
 	@Test
 	void contextLoads() {
