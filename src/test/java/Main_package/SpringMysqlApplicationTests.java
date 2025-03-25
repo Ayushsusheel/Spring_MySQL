d49636f5998b21bd8ccb1f985cffbb5f4d4e2faf
Ayush S <60178558+Ayushsusheel@users.noreply.github.com>
2025-03-25 23:29:40 +0530

    Update SpringMysqlApplicationTests.java

--- src/test/java/Main_package/SpringMysqlApplicationTests.java
+++ src/test/java/Main_package/SpringMysqlApplicationTests.java
@@ -1,6 +1,6 @@
 package Main_package;           
          
-import   org.junit.jupiter.api.Test;
+import   org.junit.jupiter.api.Test; 
 import org.springframework.boot.test.context.SpringBootTest;
  
 @SpringBootTest
